5cf2881eba0c32a3c099c0569706b167e629d8d3
Umer Farooq <umer@team.esipick.com>
2022-06-23 14:36:07 +0500

    controller & view created for category

---
+++ blog/resources/views/category/index.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.main')
+
+@section('content')
+
+<h1 class="h3 mb-4 text-gray-800">Category Control Page</h1>
+
+<nav class="navbar navbar-inverse">
+    <ul class="nav navbar-nav">
+        <li><a href="">View all categories</a></li>
+        <li><a href="category/create">Create new category</a></li>
+    </ul>
+</nav>
+
+@endsection
