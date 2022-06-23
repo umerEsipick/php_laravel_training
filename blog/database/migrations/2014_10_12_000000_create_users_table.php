e07c984e529792dca6749a5149f8d3af717e2097
Umer Farooq <umer@team.esipick.com>
2022-06-23 13:14:43 +0500

    database setup

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('admin')->default(0);
+            $table->boolean('confirmed')->default(0);
+            $table->boolean('active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
