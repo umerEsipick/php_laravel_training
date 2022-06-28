19a9b8afeb739bf1f90705a0c65bf0a7ad51adb5
Umer Farooq <umer@team.esipick.com>
2022-06-28 14:08:38 +0500

    admin password reset done

--- blog/app/Http/Controllers/UserController.php
+++ blog/app/Http/Controllers/UserController.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Validator;
+use Session;
 
 class UserController extends Controller
 {
     public function index()
     {
         return view('admin.index');
+    }
+
+    public function edit($id)
+    {
+        $users = User::find($id);
+        return view('auth.change_password')->with('users', $users);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('user/' . Auth::user()->id . '/edit')->withInput()->withErrors($validator);
+        }
+
+        $user = User::find($id);
+
+        $user->fill(['password'=>Hash::make($request->password)])->save();
+
+        Session::flash('user_update', 'Your password has been updated');
+
+        return redirect('user');
     }
 }
--- blog/resources/views/Admin/main.blade.php
+++ blog/resources/views/Admin/main.blade.php
@@ -275,9 +275,9 @@
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="{!! url('user/'.Auth::user()->id.'/edit') !!}">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
+                                    Change password
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
